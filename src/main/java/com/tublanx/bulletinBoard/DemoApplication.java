2a4b087f3bc94adf1a6ad57d25bad745a65b4ff6
Tublanx <rlgus03453@naver.com>
2022-11-30 22:54:17 +0900

    mybatis 기본 설정

--- src/main/java/com/tublanx/bulletinBoard/DemoApplication.java
+++ src/main/java/com/tublanx/bulletinBoard/DemoApplication.java
@@ -1,13 +1,31 @@
 package com.tublanx.bulletinBoard;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean(); // DB와의 연결과 SQL의 실행에 대한 모든 것을 가짐.
+		sessionFactoryBean.setDataSource(dataSource);
+
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+		sessionFactoryBean.setMapperLocations(res);
+
+		return sessionFactoryBean.getObject();
 	}
 
 }
