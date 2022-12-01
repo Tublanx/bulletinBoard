7349ce44c1d2762966f24c2f2876c42ddc868c9c
Tublanx <rlgus03453@naver.com>
2022-12-01 21:28:13 +0900

    main.html 경로로 매핑

--- src/main/java/com/tublanx/bulletinBoard/controller/PostController.java
+++ src/main/java/com/tublanx/bulletinBoard/controller/PostController.java
@@ -1,24 +1,28 @@
 package com.tublanx.bulletinBoard.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.tublanx.bulletinBoard.dao.PostDao;
-import com.tublanx.bulletinBoard.domain.PostVO;
 
-@RestController
+@Controller
 public class PostController {
 
 	@Autowired
 	PostDao postDao;
 
 	@GetMapping(value = "/main")
-	public List<PostVO> main() {
-		List<PostVO> postList = postDao.selectList();
-		return postList;
+	public String main(Model model) {
+		model.addAttribute("posts", postDao.selectList());
+		return "main";
 	}
+
+//	@GetMapping(value = "/main")
+//	public List<PostVO> main() {
+//		List<PostVO> postList = postDao.selectList();
+//		return postList;
+//	}
 
 }
