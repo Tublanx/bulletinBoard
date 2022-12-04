66ae410b30b389be2d5e204d2a0714b735341d91
Tublanx <rlgus03453@naver.com>
2022-12-04 18:25:39 +0900

    Post 테이블에 insert할 때 PostDto를 이용하기 위해 생성

---
+++ src/main/java/com/tublanx/bulletinBoard/dto/PostDto.java
@@ -0,0 +1,77 @@
+package com.tublanx.bulletinBoard.dto;
+
+import java.time.LocalDateTime;
+
+public class PostDto {
+
+	private Long postNo;
+
+	private String postName;
+
+	private String author;
+	
+	private String summary;
+	
+	private String description;
+
+	private LocalDateTime createdDate;
+
+	private LocalDateTime modifiedDate;
+
+	public Long getPostNo() {
+		return postNo;
+	}
+
+	public void setPostNo(Long postNo) {
+		this.postNo = postNo;
+	}
+
+	public String getPostName() {
+		return postName;
+	}
+
+	public void setPostName(String postName) {
+		this.postName = postName;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(LocalDateTime modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+	
+}
