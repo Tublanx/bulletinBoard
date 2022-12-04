7abc59248db944d51c553f9f993a1541f6ad07e2
Tublanx <rlgus03453@naver.com>
2022-12-04 18:24:24 +0900

    addPost 메서드 추가 (게시글 추가)

--- src/main/java/com/tublanx/bulletinBoard/dao/PostDao.java
+++ src/main/java/com/tublanx/bulletinBoard/dao/PostDao.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.tublanx.bulletinBoard.domain.PostVO;
+import com.tublanx.bulletinBoard.dto.PostDto;
 
 @Mapper
 public interface PostDao {
 
 	public List<PostVO> selectList();
+	
+	public void addPost(PostDto postDto);
 
 }
