6b1ded98330240a9c0d3958c9c4d6285c534026b
Tublanx <rlgus03453@naver.com>
2022-12-05 20:10:46 +0900

    유효성 검사하는 코드 추가

--- src/main/java/com/tublanx/bulletinBoard/controller/PostController.java
+++ src/main/java/com/tublanx/bulletinBoard/controller/PostController.java
@@ -1,14 +1,20 @@
 package com.tublanx.bulletinBoard.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tublanx.bulletinBoard.dao.PostDao;
 import com.tublanx.bulletinBoard.dto.PostDto;
 import com.tublanx.bulletinBoard.service.PostService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class PostController {
@@ -47,7 +53,14 @@ public class PostController {
 	}
 
 	@PostMapping(value = "/main/new/addpost.do")
-	public String addPost(PostDto postDto) {
+	public String addPost(@Valid PostDto postDto, Errors errors, Model model) {
+		
+		if (errors.hasErrors()) {
+			model.addAttribute("postDto", postDto);
+			
+			return "newPost";
+		}
+
 		postService.addPost(postDto);
 		return "redirect:/main";
 	}
