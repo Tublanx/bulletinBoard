1dd69e895b50bc5a16bf1a15d439c293dab23ee8
Tublanx <rlgus03453@naver.com>
2022-12-06 18:55:24 +0900

    유효성 핸들링 메서드 추가

--- src/main/java/com/tublanx/bulletinBoard/service/PostService.java
+++ src/main/java/com/tublanx/bulletinBoard/service/PostService.java
@@ -1,8 +1,10 @@
 package com.tublanx.bulletinBoard.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
 
 import com.tublanx.bulletinBoard.domain.PostVO;
 import com.tublanx.bulletinBoard.dto.PostDto;
@@ -13,5 +15,7 @@ public interface PostService {
 	public List<PostVO> selectList();
 	
 	public void addPost(PostDto postDto);
+	
+	public Map<String, String> validateHandling(Errors errors);
 	
 }
