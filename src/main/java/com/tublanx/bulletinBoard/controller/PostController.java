c738a7dd30341d7843f5f175502069bd8c849ac1
Tublanx <rlgus03453@naver.com>
2022-12-09 22:42:01 +0900

    검색 컨트롤러 구현

--- src/main/java/com/tublanx/bulletinBoard/controller/PostController.java
+++ src/main/java/com/tublanx/bulletinBoard/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.tublanx.bulletinBoard.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,8 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.tublanx.bulletinBoard.dao.PostDao;
+import com.tublanx.bulletinBoard.domain.PostVO;
 import com.tublanx.bulletinBoard.dto.PostDto;
 import com.tublanx.bulletinBoard.service.PostService;
 
@@ -48,16 +52,16 @@ public class PostController {
 	public String cancel() {
 		return "redirect:/main";
 	}
-	
+
 	@GetMapping(value = "/main/search.do")
 	public String search(@RequestParam(value = "keyword", required = false) String keyword, Model model) {
 		List<PostDto> postDtoList = postService.getSearchedList(keyword);
-		
-		model.addAttribute("postDto", postDtoList);
-		
+
+		model.addAttribute("posts", postDtoList);
+
 		return "main";
 	}
-	
+
 	@PostMapping(value = "/main/new/addpost.do")
 	public String addPost(PostDto postDto) {
 		postService.addPost(postDto);
